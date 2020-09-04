83d36ac8b8d9cbf8925edc9b0742e3cdc51fa663
TravisHeffelfinger <36177937+TravisHeffelfinger@users.noreply.github.com>
2020-09-03 19:07:45 -0600

    git branching test

--- api/db.js
+++ api/db.js
@@ -1,6 +1,6 @@
 const pg = require('pg')
 
-const connectionString = 'postgres://postgres:postgres@localhost:5432/dvdrental';
+const connectionString = 'postgres://brdogkwafzbmzy:4150ebbb93240739ebbfcb459f671bd1fe2a4c1ff0d769126506e686005b49e1@ec2-184-72-162-198.compute-1.amazonaws.com:5432/deq69vd4hv1oa2';
 
 const client = new pg.Client(connectionString);
 client.connect();
