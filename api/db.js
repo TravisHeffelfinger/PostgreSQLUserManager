46605dd30753bb3eddb565d6a7f8b289bae1431c
TravisHeffelfinger <36177937+TravisHeffelfinger@users.noreply.github.com>
2020-10-27 18:42:07 -0600

    test modification for postgres

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ const pg = require('pg')
 
 const client = new pg.Client({
     connectionString: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/deq69vd4hv1oa2',
-    ssl: process.env.DATABASE_URL ? true : false
+    ssl: {rejectUnauthorized: false} 
 });
 client.connect();
 
