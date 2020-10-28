f61058420d1a77882659ae9a5743b26b6b031373
TravisHeffelfinger <36177937+TravisHeffelfinger@users.noreply.github.com>
2020-10-27 18:27:21 -0600

    modified connection to postgres

--- api/db.js
+++ api/db.js
@@ -1,6 +1,9 @@
 const pg = require('pg')
 
-const client = new pg.Client({connectionString: process.env.DATABASE_URL, ssl: {rejectUnauthorized: false}});
+const client = new pg.Client({
+    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/deq69vd4hv1oa2',
+    ssl: process.env.DATABASE_URL ? true : false
+});
 client.connect();
 
 module.exports = client;
---
+++ postgres-demo-client/build/precache-manifest.c4d534776a098c2ae490c9da702b79d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651ad53f197b86dada2731b5c1c54838",
+    "url": "/index.html"
+  },
+  {
+    "revision": "614edb441d95a4d90628",
+    "url": "/static/css/main.6ca7d976.chunk.css"
+  },
+  {
+    "revision": "14e1b8326564715acbd0",
+    "url": "/static/js/2.f7b27640.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f7b27640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614edb441d95a4d90628",
+    "url": "/static/js/main.4456fb59.chunk.js"
+  },
+  {
+    "revision": "29b2928f49e3422a9088",
+    "url": "/static/js/runtime-main.9c617eda.js"
+  }
+]);
